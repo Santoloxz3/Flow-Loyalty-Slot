53133b7acd131bae1870e4781b1ab01e2caad019
Santoloxz3 <wolfoloxone@gmail.com>
2026-07-15 12:45:13 +0000

    Fix Netlify auto-deploy build
    Make the Tailwind PostCSS plugin available to remote builds and remove the extra autoprefixer dependency from the PostCSS config.

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,6 @@
 // postcss.config.js (versione CommonJS)
 const tailwindcss = require('@tailwindcss/postcss');
-const autoprefixer = require('autoprefixer');
 
 module.exports = {
-  plugins: [tailwindcss(), autoprefixer()],
+  plugins: [tailwindcss()],
 };
